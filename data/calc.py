f95ebad09b8e70119c633bd06d7e5cdb0c096f52
Yulun Zhang <lunjohnzhang@gmail.com>
2019-01-12 06:52:29 -0800

    code to calculate max size of matrix

--- data/calc.py
+++ data/calc.py
@@ -1,11 +1,24 @@
 import json
 from pprint import pprint
+import os
 
-with open("charts.json") as charts_data:
-    charts = json.load(charts_data)
-    print(len(charts))
+directory_in_str = "matrix"
+directory = os.fsencode(directory_in_str)
+sizes = []
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    print(filename)
+    if filename.split(".")[1] != "json":
+        continue
+    with open("matrix/" + filename)as matrix_data:
+        matrix = json.load(matrix_data)
+        sizes.append((len(matrix[0]), len(matrix)))
 
-
-with open("matrix.json") as matrix_data:
-    matrix = json.load(matrix_data)
-    print(len(matrix))
+maxM = 0
+maxN = 0
+for size in sizes:
+    if maxM < size[0]:
+        maxM = size[0]
+    if maxN < size[1]:
+        maxN = size[1]
+print(str(maxM) + " " + str(maxN))
